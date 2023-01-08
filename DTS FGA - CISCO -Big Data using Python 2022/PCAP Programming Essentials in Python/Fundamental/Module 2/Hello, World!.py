dce98d306e461edf3ff944271c2315af4e6a101f
Roni Merdiansah <91016078+Dapperson@users.noreply.github.com>
2023-01-08 22:58:32 +0700

    Add files via upload

---
+++ DTS FGA - CISCO -Big Data using Python 2022/PCAP Programming Essentials in Python/Fundamental/Module 2/Hello, World!.py
@@ -0,0 +1,16 @@
+#The print() function - the keyword arguments
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in",sep="***",end="...")
+print("Python")
+
+print("    *",
+"   * *",
+"  *   *",
+" *     *",
+"***   ***",
+"  *   *",
+"  *   *",
+"  *****",sep="\n")
+
---
+++ DTS FGA - CISCO -Big Data using Python 2022/PCAP Programming Essentials in Python/Fundamental/Module 2/Python Literals.py
@@ -0,0 +1,17 @@
+var_string = "Roni","2","3.14"
+var_int = 410000, 410_000, +410000, +410_000, -410000, -410_000
+var_float = 2.5, 0.4, -4.0, .4, 4.
+
+3E8 #Eksponen (3 x10^8)
+6.62607E-34 #Eksponen (6.62607 x 10^-34)
+print(0.0000000000000000000001) # Hasil 1e-22
+
+print(0o123) #Octal to decimal
+print(0x123) #Hexadecimal to decimal
+
+print("\"I'm\"",end=" ")
+print("\"\"learning\"\"",end=" ")
+print("\"\"\"Python\"\"\"")
+
+print(True > False)
+print(True < False)
